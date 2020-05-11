5dfd5df34307178996a162311a8febc333e5263d
Robert Twyman <robbietuk@gmail.com>
2020-05-11 15:02:12 +0100

    Small fixes and README updated

--- VoxelisedXCAT/prepare_files.sh
+++ VoxelisedXCAT/prepare_files.sh
@@ -13,20 +13,22 @@
 
 SCANNERTYPE=$1
 
-if [[ $SCANNERTYPE == "D690" ]]; then
+if [ $SCANNERTYPE = "D690" ]; then
 	echo "Preparing D690 scanner files"
 	D690_DIR="../ExamplesOfScannersMacros/D690"
-	cp $D690_DIR/csorter_D690.mac csorter.mac
-	cp $D690_DIR/digitiser_D690.mac digitiser.mac
-	cp $D690_DIR/geometry_D690.mac geometry.mac
+	cp -v $D690_DIR/csorter_D690.mac csorter.mac
+	cp -v $D690_DIR/digitiser_D690.mac digitiser.mac
+	cp -v $D690_DIR/geometry_D690.mac geometry.mac
 
-elif [[ $SCANNERTYPE == "mMR" ]]; then
+elif [ $SCANNERTYPE = "mMR" ]; then
 	echo "Preparing mMR scanner files"
 	mMR_DIR="../ExamplesOfScannersMacros/mMR"
-	cp $mMR_DIR/csorter_mMR.mac csorter.mac
-	cp $mMR_DIR/digitiser_mMR.mac digitiser.mac
-	cp $mMR_DIR/geometry_mMR.mac geometry.mac
+	cp -v $mMR_DIR/csorter_mMR.mac csorter.mac
+	cp -v $mMR_DIR/digitiser_mMR.mac digitiser.mac
+	cp -v $mMR_DIR/geometry_mMR.mac geometry.mac
 
 else
-	echo "Invalid script parse. Please indicate 'D690' or 'mMR'."
+	echo "Invalid scanner name parsed. Please indicate 'D690' or 'mMR'."
 fi
+
+exit 1
