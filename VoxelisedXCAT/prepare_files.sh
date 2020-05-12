be7775676c7f108ffcf65c61e984d11a1140f8fc
Robert Twyman <robbietuk@gmail.com>
2020-05-12 13:13:00 +0100

    Preserve copy in prepare_files and

--- VoxelisedXCAT/prepare_files.sh
+++ VoxelisedXCAT/prepare_files.sh
@@ -16,16 +16,16 @@ SCANNERTYPE=$1
 if [ $SCANNERTYPE = "D690" ]; then
 	echo "Preparing D690 scanner files"
 	D690_DIR="../ExamplesOfScannersMacros/D690"
-	cp -v $D690_DIR/csorter_D690.mac GateSubMacros/csorter.mac
-	cp -v $D690_DIR/digitiser_D690.mac GateSubMacros/digitiser.mac
-	cp -v $D690_DIR/geometry_D690.mac GateSubMacros/geometry.mac
+	cp -vp $D690_DIR/csorter_D690.mac GateSubMacros/csorter.mac
+	cp -vp $D690_DIR/digitiser_D690.mac GateSubMacros/digitiser.mac
+	cp -vp $D690_DIR/geometry_D690.mac GateSubMacros/geometry.mac
 
 elif [ $SCANNERTYPE = "mMR" ]; then
 	echo "Preparing mMR scanner files"
 	mMR_DIR="../ExamplesOfScannersMacros/mMR"
-	cp -v $mMR_DIR/csorter_mMR.mac GateSubMacros/csorter.mac
-	cp -v $mMR_DIR/digitiser_mMR.mac GateSubMacros/digitiser.mac
-	cp -v $mMR_DIR/geometry_mMR.mac GateSubMacros/geometry.mac
+	cp -vp $mMR_DIR/csorter_mMR.mac GateSubMacros/csorter.mac
+	cp -vp $mMR_DIR/digitiser_mMR.mac GateSubMacros/digitiser.mac
+	cp -vp $mMR_DIR/geometry_mMR.mac GateSubMacros/geometry.mac
 
 else
 	echo "Invalid scanner name parsed. Please indicate 'D690' or 'mMR'."
