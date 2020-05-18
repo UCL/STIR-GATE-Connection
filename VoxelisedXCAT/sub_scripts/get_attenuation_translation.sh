60378215bdf561585c5513e8229dbc8df6c28ed4
Robert Twyman <robbietuk@gmail.com>
2020-05-18 20:27:21 +0100

    get_attenuation_translation now based on first and last edges.
    This makes this script consistent with get_source_position. Slightly convoluted method but hopefully readable.

--- VoxelisedXCAT/sub_scripts/get_attenuation_translation.sh
+++ VoxelisedXCAT/sub_scripts/get_attenuation_translation.sh
@@ -10,15 +10,20 @@ if [ $# -ne 1 ]; then
   exit 1
 fi
 
-## Get voxel size for x,y dimensions
-VoxelSizes=`list_image_info $ImageFilename| awk -F: '/Voxel-size in mm/ {print $2}'|tr -d '{}'|awk -F, '{print $3, $2, $1}'` 1>&2	
-VoxelSizeX=`echo ${VoxelSizes} |awk '{print $1}'`
-VoxelSizeY=`echo ${VoxelSizes} |awk '{print $2}'`
+## Get first and last edge positions
+FirstEdges=`list_image_info $ImageFilename| awk -F: '/Physical coordinate of first edge in mm/ {print $2}'|tr -d '{}'|awk -F, '{print $3, $2, $1}'` 1>&2
+LastEdges=`list_image_info $ImageFilename| awk -F: '/Physical coordinate of last edge in mm/ {print $2}'|tr -d '{}'|awk -F, '{print $3, $2, $1}'` 1>&2
 
-## Translation is 1/2 voxel size in x,y
-TranslationX=$(echo "$VoxelSizeX" | awk '{print -($1/2)}')
-TranslationY=$(echo "$VoxelSizeY" | awk '{print -($1/2)}')
-TranslationZ=0.  ## There should not be a requirement for z translation.
+## Extract x,y first and last edges
+FirstEdgeX=$(echo "$FirstEdges" | awk '{print $1}')
+LastEdgeX=$(echo "$LastEdges" | awk '{print $1}')
+FirstEdgeY=$(echo "$FirstEdges" | awk '{print $2}')
+LastEdgeY=$(echo "$LastEdges" | awk '{print $2}')
+
+## Compute Translation
+TranslationX=$(echo "$FirstEdgeX $LastEdgeX" | awk '{print ($1+$2)/2}')
+TranslationY=$(echo "$FirstEdgeY $LastEdgeY" | awk '{print ($1+$2)/2}')
+TranslationZ=0.  ## Z translation should be zero.
 
 ## Used for return.
 echo $TranslationX $TranslationY $TranslationZ
