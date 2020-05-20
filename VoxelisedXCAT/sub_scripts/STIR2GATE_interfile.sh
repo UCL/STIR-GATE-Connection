78c0be1870370a420cdfb2a9199aa11aa8d02dd3
Robert Twyman <robbietuk@gmail.com>
2020-05-20 16:51:40 +0100

    Adds STIR2GATE_interfile.sh conversion script.

---
+++ VoxelisedXCAT/sub_scripts/STIR2GATE_interfile.sh
@@ -0,0 +1,37 @@
+#! /bin/sh
+## AUTHOR: Robert Twyman
+## Copyright (C) 2020 University College London
+## Licensed under the Apache License, Version 2.0
+
+## Script take a STIR image header filename and converts it into a GATE
+## compatable .h33 file. Additionally, "!number of slices" and 
+## "slice thickness (pixels)" fields are added.
+
+
+if [ $# -ne 2 ]; then
+  echo "Usage:"$0 "GATEOutputFilename STIRInputFilename" 1>&2
+  echo " - GATEOutputFilename: should have '.h33' suffix."
+  echo " - STIRInputFilename: should have '.hv' suffix"
+  exit 0
+fi
+
+GATEFilename=$1
+STIRFilename=$2
+
+## Make copy for input
+cp $STIRFilename $GATEFilename
+
+## Get the number of slices = Number of voxels in z
+NumberOfSlices=`list_image_info $GATEFilename | awk -F: '/Number of voxels / {print $2}'|tr -d '{}'|awk -F, '{print $1}'` 1>&2
+## Get slice thickness in z
+SliceThickness=`list_image_info $GATEFilename | awk -F: '/Voxel-size in mm / {print $2}'|tr -d '{}'|awk -F, '{print $1}'` 1>&2
+## Get the line number to insert the text into
+LineNum=$( grep -n "!END OF INTERFILE" $GATEFilename | cut -d : -f 1 )
+
+# Add $NumberOfSlices and $SliceThickness at $LineNum
+sed -i '' $LineNum'i\
+!number of slices := '$NumberOfSlices'\
+slice thickness (pixels) := '$SliceThickness'
+' $GATEFilename
+
+exit 0
--- VoxelisedXCAT/sub_scripts/generate_STIR_GATE_images.sh
+++ VoxelisedXCAT/sub_scripts/generate_STIR_GATE_images.sh
@@ -6,14 +6,15 @@
 ## - Script used to generate activity and attenuation images in STIR 
 ##   from parameter files.
 ## - A modification is made to the scale of the attenuation file for GATE.
-## - The script copies the .hv files as .h33 (for GATE) and populates 
-##   NumberOfSlices and SliceThickness in the file.
+## - Converts header file (.hv) to GATE compatable header (.h33) using
+##   STIR2GATE_interfile.sh.
 ## - Copies .h33 and .v files to STIRGATEHome directory. This step is done
 ##   for GATE to read images.
 
 
 if [ $# -ne 2 ]; then
   echo "Usage:"$0 "ActivityPar AttenuationPar" 1>&2
+  echo "Returns Activity and Attenuation filenames."
   exit 0
 fi
 
@@ -33,25 +34,9 @@ AttenuationFilenameGATE=$AttenuationFilename"_GATE"
 ## Process my_uniform_cylinder.hv my_atten_image_GATE.hv into .h33 files
 ## and add "!number of slices :=" and "slice thickness (pixels) :=" fields.
 for Filename in $ActivityFilename $AttenuationFilenameGATE; do
-  cp $Filename".hv" $Filename".h33"
-
-  ## Get the number of slices = Number of voxels in z
-  NumberOfSlices=`list_image_info $Filename".h33" | awk -F: '/Number of voxels / {print $2}'|tr -d '{}'|awk -F, '{print $1}'` 1>&2
-  ## Get slice thickness in z
-  SliceThickness=`list_image_info $Filename".h33" | awk -F: '/Voxel-size in mm / {print $2}'|tr -d '{}'|awk -F, '{print $1}'` 1>&2
-  ## Get the line number to insert the text into
-  LineNum=$( grep -n "!END OF INTERFILE" $Filename".h33" | cut -d : -f 1 )
-
-  # Add $NumberOfSlices and $SliceThickness at $LineNum
-  sed -i '' $LineNum'i\
-  !number of slices := '$NumberOfSlices'\
-  slice thickness (pixels) := '$SliceThickness'
-  ' $Filename".h33"
-
-  ## Copy the files to the main directory.
+  sh $STIRGATEHome/sub_scripts/STIR2GATE_interfile.sh $Filename".h33" $Filename".hv" 
   cp $Filename".h33" $STIRGATEHome
   cp $Filename".v" $STIRGATEHome
-
 done
 
 echo $ActivityFilename".h33" $AttenuationFilenameGATE".h33"
