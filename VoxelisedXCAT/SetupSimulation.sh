4e5e987158a3ff5a75bcba0ff41704f4e0ec661a
Robert Twyman <robbietuk@gmail.com>
2020-06-25 20:14:14 +0100

    Creation of SetupDmap.mac script. Removed dmap creation from phantomReg

--- VoxelisedXCAT/SetupSimulation.sh
+++ VoxelisedXCAT/SetupSimulation.sh
@@ -2,42 +2,55 @@
 
 # This script will:
 #	- Copy files from correct scanner geometry (runs sub_scripts/prepare_scanner.sh)
-#	- OPTIONAL: Creates voxelised phantom from parameter files
+#	- OPTIONAL: If ACTIVITY and ATTENUATION have "par" extension, use STIR to generate voxelised phantom
 #	- Opens Gate and creates DMAP for phantom (this should only be run once per phantom.)
-
 
 
 # {add parameter checks}
 if [ $# != 4 ]
 then
 	echo "SetupSimulation: Setup scanner, voxelised phantom files and dmap"
-	echo "Usage: ScannerType StoreRootFilesDirectory ActivityPar AttenuationPar"
+	echo "Usage: ScannerType StoreRootFilesDirectory Activity(Par/Filename) Attenuation(Par/Filename)"
 	exit 1
 fi
 
 # Parameters
-ScannerType
-StoreRootFilesDirectory
-
-ActivityPar
-AttenuationPar
+ScannerType=$1
+StoreRootFilesDirectory=$2
+ACTIVITY=$3
+ATTENUATION=$4
 
 
 ## Get the scanner files into GATESubMacros directory.
-./sub_scripts/prepare_scanner_files.sh $ScannerType $StoreRootFilesDirectory
+./sub_scripts/prepare_scanner_files.sh $ScannerType
 
 
+# Check extension of ACTIVITY and ATTENUATION
+if [ "${ACTIVITY##*.}" == "par"  ] && [ "${ATTENUATION##*.}" == "par"  ]
+then
+	# If "par", then generate voxelised phantom using STIR
+	ACTIVITYPAR=$ACTIVITY
+	ATTENUATIONPAR=$ATTENUATION
+	echo "Generating voxelised phantom"
+	SourceFilenames=`sub_scripts/generate_STIR_GATE_images.sh $ACTIVITYPAR $ATTENUATIONPAR 2>/dev/null`
+	## Get activity and attenuation filenames from $SourceFilenames and replace
+	ActivityFilename=`echo ${SourceFilenames} |awk '{print $1}'`
+	AttenuationFilename=`echo ${SourceFilenames} |awk '{print $2}'`
+else
+	# Otherwise, then generate voxelised phantom using STIR
+	ActivityFilename=$ACTIVITY
+	AttenuationFilename=$ATTENUATION
+fi
 
+# Check AttenuationFilename has extension "h33"
+if [ "${AttenuationFilename##*.}" != "h33"  ]
+then
+	echo "SetupSimulation: AttenuationFilename does not have suffix *.h33"
+	echo $AttenuationFilename
+	exit 1
+fi
 
-## Generate example STIR activity and attenuation images and copy to main dir.
-ActivityPar=images/input/generate_uniform_cylinder.par
-AttenuationPar=images/input/generate_atten_cylinder.par
+## Run a very short simulation to setup and save dmap.hdr
+Gate GATESubMacros/SetupDmap.mac -a [AttenuationFilename,$AttenuationFilename]
 
-SourceFilenames=`sub_scripts/generate_STIR_GATE_images.sh $ActivityPar $AttenuationPar 2>/dev/null`
-## Get activity and attenuation filenames from $SourceFilenames
-ActivityFilename=`echo ${SourceFilenames} |awk '{print $1}'`
-AttenuationFilename=`echo ${SourceFilenames} |awk '{print $2}'`
-
-
-# Run Gate DMAP setup
-Gate SetupDmap.mac
+echo "SetupSimulation Complete"
