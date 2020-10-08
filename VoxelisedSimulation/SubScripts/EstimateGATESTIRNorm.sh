636ebdefc0903d08ebb31b67b4813944c95e85a8
Robert Twyman <robbietuk@gmail.com>
2020-10-08 14:51:07 +0100

    Add span argument and functionality to EstimateGATESTIRNorm

--- VoxelisedSimulation/SubScripts/EstimateGATESTIRNorm.sh
+++ VoxelisedSimulation/SubScripts/EstimateGATESTIRNorm.sh
@@ -8,14 +8,17 @@
 ## The current standard to do this is to forward project, in STIR and GATE (run simulations), a cylindrical activity, the size of the scanner FOV, without attenuation.
 ## The MeasuredData (sinogram) is obtained by unlisting the GATE output with the exclusion of randoms and scatter.
 
+## Span: This should be an positive integer that saves an additional sinogram with the indicated span. 
+## If no span changes is wanted, please input 1.
+
 ## This script forward projects the same activity cylinder in SITR to obtain model_data. 
 ## The script will estimate the norm factors (norm_sino.hs) using STIR functionality. 
 ## See find_ML_normfactors3D and apply_normfactors3D for more information
 
 ## PARAMETERS
 
-if [ $# != 3 ]; then
-	echo "Usage: EstimateGATESTIRNorm.sh OutputFilename MeasuredData FOVCylindricalActivityVolumeFilename"
+if [ $# != 4 ]; then
+	echo "Usage: EstimateGATESTIRNorm.sh OutputFilename MeasuredData FOVCylindricalActivityVolumeFilename Span"
 	exit 1
 fi 
 
@@ -25,6 +28,7 @@ trap "echo ERROR in $0" ERR
 OutputFilename=$1
 MeasuredData=$2
 FOVCylindricalActivityVolumeFilename=$3
+span=$4
 
 
 ## ML Normfactors loop numbers (Hardcoded for now)
@@ -56,8 +60,15 @@ stir_math -s --including-first --times-scalar 0 --add-scalar 1 ones.hs $model_da
 echo "apply_normfactors3D"
 apply_normfactors3D $eff_factors $factors ones.hs 1 $outer_iters $eff_iters
 
+## Creates the span-1 normalisation sinogram
 echo "inverting the eff_factors to get norm"
 stir_math -s --including-first --power -1 $OutputFilename $eff_factors
+
+## Creates the span-n normalisation sinogram if $span > 1
+if [ span != 1]; then
+	SSRB span${span}_${eff_factors} ${eff_factors} $span 1 0
+	stir_math -s --including-first --power -1 span${span}_$OutputFilename span${span}_$eff_factors
+fi
 
 rm ones.hs
 rm ones.s
