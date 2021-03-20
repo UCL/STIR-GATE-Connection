308142c019719f959b8c8684131f9d4d7797a456
Robert Twyman <robbietuk@gmail.com>
2021-03-20 07:00:55 +0000

    Corrections to factors string and an echo

--- VoxelisedSimulation/SubScripts/EstimateRandomsFromDelayed.sh
+++ VoxelisedSimulation/SubScripts/EstimateRandomsFromDelayed.sh
@@ -19,13 +19,13 @@ OutputFilename=$1
 DelayedData=$2 ## INPUT: Delayed coincidences sinogram
 
 ## factors are a temporary file created by find_ML_singles_from_delayed [deleted by cleanup]
-factors=randoms_factors
+factors=singles_from_delayed
 num_iterations=10
 
 echo "find_ML_singles_from_delayed"
 find_ML_singles_from_delayed ${factors} ${DelayedData} ${num_iterations} < /dev/null
 
-echo "apply_normfactors3D"
+echo "construct_randoms_from_singles"
 construct_randoms_from_singles ${OutputFilename} ${factors} ${DelayedData} ${num_iterations}
 
 cleanup=1
