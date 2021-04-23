2cde4e96853938eff3c6c97b90c7dd4cbbbd3ee2
Robert Twyman <robbietuk@gmail.com>
2021-04-23 16:11:01 -0700

    OSEM reconstruction example

---
+++ ExampleReconstruction/ExampleReconstruction.sh
@@ -0,0 +1,21 @@
+
+# ExampleReconstruction.sh script perform OSEM as an example reconstruction for GATE data.
+#  The required inputs are as follow:
+
+
+Prompts=$1
+AdditiveSinogram=$2
+MultFactors=$3
+
+echo "The reconstruction files are as follow"
+echo "    Coincidence Data[Prompts] = ${Prompts}"
+echo "    Additive Sinogram[AdditiveSinogram] = ${AdditiveSinogram}"
+echo "    Multiplicative Factors [MultFactors] = ${MultFactors}"
+
+if [ $# != 3 ]; then 
+	echo "USAGE: $0 Coincidence Data[Prompts] AdditiveSinogram MultiplicativeFactors[MultFactors]"
+
+export Prompts AdditiveSinogram MultFactors SGCPATH
+
+
+OSMAPOSL ${SGCPATH}/ExampleReconstruction/OSEM.par
