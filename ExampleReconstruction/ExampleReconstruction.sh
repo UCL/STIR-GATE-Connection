70bdaf859ab30352458265b39c47b27336a44a7a
Robert Twyman <robbietuk@gmail.com>
2021-04-28 11:13:18 -0700

    Documentation changes

--- ExampleReconstruction/ExampleReconstruction.sh
+++ ExampleReconstruction/ExampleReconstruction.sh
@@ -6,18 +6,21 @@
 
 # ExampleReconstruction.sh script perform OSEM as an example reconstruction for GATE data.
 #  The required inputs are as follow:
-#		Coincidence Data 
-#		Additive Sinogram 
-#		Multiplicative Factors"
+#		Coincidence Data Filename
+#		Additive Sinogram Filename
+#		Multiplicative Factors Filename
 # Optional inputs are:
 # 		The number of subsets
 # 		The number of subiterations
 
-Prompts=$1
-AdditiveSinogram=$2
-MultFactors=$3
 
-# Default for optional variables
+Prompts=$1 # This should be the unlisted output of the GATE simulations
+AdditiveSinogram=$2 # The estimated contributions due to random and scattered events
+MultFactors=$3 # The geometric, physical, and attuation factors of the simulation.
+# Note, the AdditiveSinogram and MultFactors can be computed using 
+# the `ComputePoissonDataCorrections.sh` script.
+
+# Default for optional variables for the reconstruction
 NumSubsets=8
 NumSubiterations=40
 
@@ -29,7 +32,7 @@ echo " "
 ## Usage message
 if [ $# -lt 3 ]; then 
 	echo "USAGE: $0 CoincidenceData AdditiveSinogram MultiplicativeFactors [NumSubsets NumSubiterations]"
-	echo "  Where NumSubset and NumSubiterations are optional."
+	echo "  Where NumSubsets and NumSubiterations are optional."
 fi
 
 # Handle optional arguemnts
@@ -38,14 +41,15 @@ if [ $# -ge 5 ]; then
 	NumSubiterations=$5
 fi
 
-
-echo "The reconstruction files provided are as follow"
+# Print configuration to terminal
+echo "The reconstruction files provided are as follow:"
 echo "  Coincidence Data[Prompts] = ${Prompts}"
 echo "  Additive Sinogram[AdditiveSinogram] = ${AdditiveSinogram}"
 echo "  Multiplicative Factors [MultFactors] = ${MultFactors}"
 echo "  Number of Subsets [NumSubsets] = ${NumSubsets}"
 echo "  Number of Subiterations [NumSubiterations] = ${NumSubiterations}"
 
+# Export variables for the parameter files
 export Prompts AdditiveSinogram MultFactors NumSubsets NumSubiterations
 
 echo "Performing image reconstruction!"
