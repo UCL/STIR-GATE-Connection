7dd367bdbbe006a9a03e11d54798c7f05d2484ec
Robert Twyman <robbietuk@gmail.com>
2021-04-28 13:42:37 -0700

    Final reconstruction example documentation adjustments

--- ExampleReconstruction/ExampleReconstruction.sh
+++ ExampleReconstruction/ExampleReconstruction.sh
@@ -3,31 +3,26 @@
 ## Copyright (C) 2021 University College London
 ## Licensed under the Apache License, Version 2.0
 
-
-# ExampleReconstruction.sh script perform OSEM as an example reconstruction for GATE data.
-#  The required inputs are as follow:
-#		Coincidence Data Filename
-#		Additive Sinogram Filename
-#		Multiplicative Factors Filename
+# This script performs OSEM reconstruction for GATE data.
+# The required inputs are as follow:
+#   Coincidence Data Filename
+#   Additive Sinogram Filename
+#   Multiplicative Factors Filename
 # Optional inputs are:
-# 		The number of subsets
-# 		The number of subiterations
+#   The number of subsets
+#   The number of subiterations
 
 
+## Setup of variables:
 Prompts=$1 # This should be the unlisted output of the GATE simulations
 AdditiveSinogram=$2 # The estimated contributions due to random and scattered events
 MultFactors=$3 # The geometric, physical, and attuation factors of the simulation.
 # Note, the AdditiveSinogram and MultFactors can be computed using 
 # the `ComputePoissonDataCorrections.sh` script.
 
-# Default for optional variables for the reconstruction
+# Defaults for reconstruction optional variables
 NumSubsets=8
 NumSubiterations=40
-
-echo " "
-echo "Running ExampleReconstruction.sh"
-echo " "
-
 
 ## Usage message
 if [ $# -lt 3 ]; then 
@@ -42,12 +37,13 @@ if [ $# -ge 5 ]; then
 fi
 
 # Print configuration to terminal
-echo "The reconstruction files provided are as follow:"
+echo " "
+echo "The reconstruction configuration is:"
 echo "  Coincidence Data[Prompts] = ${Prompts}"
 echo "  Additive Sinogram[AdditiveSinogram] = ${AdditiveSinogram}"
 echo "  Multiplicative Factors [MultFactors] = ${MultFactors}"
 echo "  Number of Subsets [NumSubsets] = ${NumSubsets}"
-echo "  Number of Subiterations [NumSubiterations] = ${NumSubiterations}"
+echo "  Number of subiterations [NumSubiterations] = ${NumSubiterations}"
 
 # Export variables for the parameter files
 export Prompts AdditiveSinogram MultFactors NumSubsets NumSubiterations
@@ -55,3 +51,5 @@ export Prompts AdditiveSinogram MultFactors NumSubsets NumSubiterations
 echo "Performing image reconstruction!"
 OSMAPOSL ${SGCPATH}/ExampleReconstruction/OSEM.par
 echo "Image reconstruction complete!"
+exit 0
+
