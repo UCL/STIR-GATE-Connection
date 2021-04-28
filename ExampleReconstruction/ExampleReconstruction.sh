b3889537e8a077b078372468f2ba916fdebf008c
Robert Twyman <robbietuk@gmail.com>
2021-04-28 11:00:30 -0700

    ExampleReconstruction Num Subsets and Subiterations are optional variables

--- ExampleReconstruction/ExampleReconstruction.sh
+++ ExampleReconstruction/ExampleReconstruction.sh
@@ -9,21 +9,44 @@
 #		Coincidence Data 
 #		Additive Sinogram 
 #		Multiplicative Factors"
-
-if [ $# != 3 ]; then 
-	echo "USAGE: $0 CoincidenceData[Prompts] AdditiveSinogram MultiplicativeFactors[MultFactors]"
-fi
+# Optional inputs are:
+# 		The number of subsets
+# 		The number of subiterations
 
 Prompts=$1
 AdditiveSinogram=$2
 MultFactors=$3
 
-echo "The reconstruction files provided are as follow"
-echo "    Coincidence Data[Prompts] = ${Prompts}"
-echo "    Additive Sinogram[AdditiveSinogram] = ${AdditiveSinogram}"
-echo "    Multiplicative Factors [MultFactors] = ${MultFactors}"
+# Default for optional variables
+NumSubsets=8
+NumSubiterations=40
 
-export Prompts AdditiveSinogram MultFactors SGCPATH
+echo " "
+echo "Running ExampleReconstruction.sh"
+echo " "
+
+
+## Usage message
+if [ $# -lt 3 ]; then 
+	echo "USAGE: $0 CoincidenceData AdditiveSinogram MultiplicativeFactors [NumSubsets NumSubiterations]"
+	echo "  Where NumSubset and NumSubiterations are optional."
+fi
+
+# Handle optional arguemnts
+if [ $# -ge 5 ]; then
+	NumSubsets=$4
+	NumSubiterations=$5
+fi
+
+
+echo "The reconstruction files provided are as follow"
+echo "  Coincidence Data[Prompts] = ${Prompts}"
+echo "  Additive Sinogram[AdditiveSinogram] = ${AdditiveSinogram}"
+echo "  Multiplicative Factors [MultFactors] = ${MultFactors}"
+echo "  Number of Subsets [NumSubsets] = ${NumSubsets}"
+echo "  Number of Subiterations [NumSubiterations] = ${NumSubiterations}"
+
+export Prompts AdditiveSinogram MultFactors NumSubsets NumSubiterations
 
 echo "Performing image reconstruction!"
 OSMAPOSL ${SGCPATH}/ExampleReconstruction/OSEM.par
