6df3eb91329747de35b4b2341ff842885ba98bdc
rmaprrt <rmaprrt@login12.myriad.ucl.ac.uk>
2021-07-25 17:08:38 +0100

    this_SGC add to /home/rmaprrt/bin/geant4_bin/install/bin:/home/rmaprrt/bin/boost_bin/install/include:/shared/ucl/apps/cuda/10.1.243/gnu-4.9.2/bin:/shared/ucl/apps/cuda/10.1.243/gnu-4.9.2/libnsight:/shared/ucl/apps/openmpi/ucl-wrapper-ompi3+-ucx/bin:/shared/ucl/apps/openmpi/4.0.3/gnu-4.9.2/bin:/shared/ucl/apps/bison/3.0.4/gnu-4.9.2/bin:/shared/ucl/apps/ucx/1.8.0/gnu-4.9.2/bin:/shared/ucl/apps/binutils/2.29.1/gnu-4.9.2/bin:/shared/ucl/apps/numactl/2.0.12/bin:/home/rmaprrt/.python3local/bin:/shared/ucl/apps/python/bundles/python38-5.0.0/venv/bin:/shared/ucl/apps/openblas/0.3.7-serial/gnu-4.9.2/bin:/shared/ucl/apps/python/3.8.6/gnu-4.9.2/bin:/shared/ucl/apps/ROOT/6.04.00/gnu-4.9.2/bin:/shared/ucl/apps/gsl/1.16/gcc/bin:/shared/ucl/apps/fftw/3.3.4/gnu-4.9.2/bin:/shared/ucl/apps/ecj/4.9/gnu-4.9.2:/shared/ucl/apps/cluster-bin:/shared/ucl/apps/cluster-scripts:/shared/ucl/apps/mrxvt/0.5.4/bin:/shared/ucl/apps/tmux/2.2/gnu-4.9.2/bin:/shared/ucl/apps/emacs/26.3/bin:/shared/ucl/apps/giflib/5.1.1/gnu-4.9.2/bin:/shared/ucl/apps/dos2unix/7.3/gnu-4.9.2/bin:/shared/ucl/apps/NEdit/5.6-Aug15/bin:/shared/ucl/apps/nano/2.4.2/gnu-4.9.2//bin:/shared/ucl/apps/GERun:/shared/ucl/apps/screen/4.2.1/bin:/shared/ucl/apps/subversion/1.8.13/bin:/shared/ucl/apps/apr-util/1.5.4/bin:/shared/ucl/apps/apr/1.5.2/bin:/shared/ucl/apps/git/2.19.1/gnu-4.9.2/bin:/shared/ucl/apps/flex/2.5.39/gnu-4.9.2/bin:/shared/ucl/apps/cmake/3.19.1/gnu-4.9.2/bin:/shared/ucl/apps/gcc/4.9.2/bin:/opt/sge/bin:/opt/sge/bin/lx-amd64:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/opt/ibutils/bin:/home/rmaprrt/bin/ITK/install/lib/cmake/ITK-4.13:/home/rmaprrt/bin/swig/install/bin:/home/rmaprrt/bin/STIR_bin/ZeljkoCustomBranch/install/bin:/home/rmaprrt/bin/STIR_bin/ZeljkoCustomBranch/install/python::/home/rmaprrt/bin/parallelproj_bin/install:/home/rmaprrt/bin/Gate_bin/install/bin:/home/rmaprrt/bin/Cluster/inm-Projects/projects/Cluster:/home/rmaprrt/projects/SOPET:/home/rmaprrt/projects/SOPET/Cluster_scripts:/home/rmaprrt/.local/bin:/home/rmaprrt/bin and add major directory enviromental variables

--- this_SGC.sh
+++ this_SGC.sh
@@ -1,3 +1,13 @@
 # This file should be sourced, e.g. `source this_SGC.sh`
+
 SGCPATH=$(cd "$(dirname "$0")"; pwd)/$(basename "$1")
-export SGCPATH
+SGCVoxelisedSimulation="${SGCPATH}/VoxelisedSimulation"
+
+export SGCPATH SGCVoxelisedSimulation
+export PATH=${SGCPATH}:${PATH}
+
+echo " "
+echo "SGCPATH enviromental variable is set as:"
+echo "  ${SGCPATH}"
+echo "and adding it to PATH."
+echo " "
