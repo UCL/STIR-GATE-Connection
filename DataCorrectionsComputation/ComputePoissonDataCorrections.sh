67eb666753f88cb6d1b6b16ee11e8fa4ba3ed7c9
robbietuk <robbietuk@gmail.com>
2022-11-25 07:37:05 -0800

    Correct documentation in ComputePoissonDataCorrections #92

--- DataCorrectionsComputation/ComputePoissonDataCorrections.sh
+++ DataCorrectionsComputation/ComputePoissonDataCorrections.sh
@@ -19,7 +19,7 @@
 #  then this script will flip the image in the z axis, because GATE seems to do that...
 
 ## Verbose usage:
-# 	0:  EstimateScatter.sh 						- Script name
+# 	0:  ComputePoissonDataCorrections.sh		- Script name
 #	1:  OutputMultiplicativeFactorsFilename		- Output filename of the multiplicative factors
 # 	2:  OutputAdditiveSinogramFilename			- Output filename of the additive sinogram
 #	3:  CoincidenceData[sino_input]				- Coincidence data sinogram filename
